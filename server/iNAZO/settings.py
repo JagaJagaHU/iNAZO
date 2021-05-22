dba873d864696a4f97b9ed74d668b5733735c2b9
karintou8710 <karintou8710@gmail.com>
2021-05-22 17:25:28 +0900

    add static_root

--- server/iNAZO/settings.py
+++ server/iNAZO/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'api.paginations.SizePagination',
